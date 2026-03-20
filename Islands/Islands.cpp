8ef729f64ae673033a25e1ea67c8dff9a9d51f99
Ase <tdruesne05@gmail.com>
2026-03-20 13:27:20 +0100

    Les iles elles se comptent

--- Islands/Islands.cpp
+++ Islands/Islands.cpp
@@ -19,7 +19,52 @@
 
 int count_islands(std::vector<std::vector<char>>& grid)
 {
-	return -1;
+    int islands = 0;
+
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[i].size(); j++)
+        {
+            if (grid[i][j] == '1')
+            {
+                islands++;
+                grid[i][j] = '0';
+
+                std::vector<std::pair<int, int>> stack;
+                stack.push_back({i, j});
+
+                while (!stack.empty())
+                {
+                    std::pair<int, int> current = stack.back();
+                    int x = current.first;
+                    int y = current.second;
+                    stack.pop_back();
+
+                    if (x > 0 && grid[x - 1][y] == '1')
+                    {
+                        stack.push_back({x - 1, y});
+                        grid[x - 1][y] = '0';
+                    }
+                    if (x < grid.size() - 1 && grid[x + 1][y] == '1')
+                    {
+                        stack.push_back({x + 1, y});
+                        grid[x + 1][y] = '0';
+                    }
+                    if (y > 0 && grid[x][y - 1] == '1')
+                    {
+                        stack.push_back({x, y - 1});
+                        grid[x][y - 1] = '0';
+                    }
+                    if (y < grid[x].size() - 1 && grid[x][y + 1] == '1')
+                    {
+                        stack.push_back({x, y + 1});
+                        grid[x][y + 1] = '0';
+                    }
+                }
+            }
+        }
+    }
+    return islands;
 }
 
 int main()
